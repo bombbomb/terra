78a30f32842bda931f0f698872fe23fec9045032
spicykoala <jeremykelly@bombbomb.com>
2017-07-24 23:10:58 -0600

    lambda_zip_express_bundle: -- basically the same as the express bundle, but accepts an already zipped file instead of handling the npm install and zip.

---
+++ lambda_zip_express_bundle/lambda.tf
@@ -0,0 +1,21 @@
+resource "aws_lambda_function" "current" {
+  filename         = "${var.lambda_path}"
+  function_name    = "${var.config["prefix"]}-${var.subdomain}"
+  role             = "${var.lambda_role_arn}"
+  handler          = "index.handler"
+  source_code_hash = "${var.lambda_hash}"
+  runtime          = "${var.lambda_runtime}"
+  memory_size      = "${var.lambda_memory_size}"
+  timeout          = "${var.lambda_timeout}"
+  environment {
+    variables = "${var.lambda_environment_variables}"
+  }
+}
+
+resource "aws_lambda_permission" "current" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.current.arn}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.current.id}/*/*"
+}
