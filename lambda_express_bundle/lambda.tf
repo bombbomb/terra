d194590dfd537f547a4307022a0b8c481c6dee98
spicykoala <jeremykelly@bombbomb.com>
2017-12-12 13:13:41 -0700

    Run optional npm command 'lighthouse-build' if available.

--- lambda_express_bundle/lambda.tf
+++ lambda_express_bundle/lambda.tf
@@ -27,7 +27,7 @@ resource "null_resource" "npm" {
   }
 
   provisioner "local-exec" {
-    command = "cd ${var.lambda_path} && npm install"
+    command = "cd ${var.lambda_path} && npm install && npm run lighthouse-build --if-present"
   }
 }
 
