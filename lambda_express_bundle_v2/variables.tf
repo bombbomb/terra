8b9bb147f06700a8d4a3131c703f4308a0f76a67
spicykoala <jeremykelly@bombbomb.com>
2017-12-12 09:29:12 -0700

    Creating a V2 express bundle that should support binary zipping.

---
+++ lambda_express_bundle_v2/variables.tf
@@ -0,0 +1,24 @@
+variable "config" { type = "map" }
+
+variable "subdomain" {}
+variable "lambda_role_arn" {}
+variable "lambda_path" {}
+
+variable "authorization" { default = "NONE" }
+
+variable "lambda_runtime" { default = "nodejs6.10"}
+variable "lambda_memory_size" { default = "128" }
+variable "lambda_timeout" { default = "30" }
+variable "lambda_environment_variables" {
+  type = "map"
+  default = {
+    CREATED_BY = "lambda_express_bundle"
+  }
+}
+variable "description" { default = "" }
+
+variable "request_parameters" {
+  type = "map"
+  default = {
+  }
+}
