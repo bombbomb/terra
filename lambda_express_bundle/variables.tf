2ef7199b0c1e088f6f668b3a0aedd48ce362bad1
spicykoala <jeremykelly@bombbomb.com>
2018-01-12 10:32:14 -0700

    sub_subdomain should not be required.

--- lambda_express_bundle/variables.tf
+++ lambda_express_bundle/variables.tf
@@ -1,7 +1,7 @@
 variable "config" { type = "map" }
 
 variable "subdomain" {}
-variable "sub_subdomain" {}
+variable "sub_subdomain" { default = "" }
 variable "certificate_arn" { default = "" }
 variable "lambda_role_arn" {}
 variable "lambda_path" {}
