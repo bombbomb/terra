5aeea7f3cc9723bf303807dcafa610bf48c24551
spicykoala <jeremykelly@bombbomb.com>
2018-03-21 15:27:02 -0600

    Support sub-sub domains for production branches.

--- lambda_express_bundle/api_gateway.tf
+++ lambda_express_bundle/api_gateway.tf
@@ -1,6 +1,12 @@
 data "aws_caller_identity" "current" {}
-data "aws_region" "current" { current = true }
-data "aws_iam_role" "lambda_role" { role_name = "${var.config["prefix"]}-AppRole" }
+
+data "aws_region" "current" {
+  current = true
+}
+
+data "aws_iam_role" "lambda_role" {
+  role_name = "${var.config["prefix"]}-AppRole"
+}
 
 # API Gateway
 resource "aws_api_gateway_rest_api" "current" {
@@ -15,9 +21,9 @@ resource "aws_api_gateway_resource" "all_paths" {
 }
 
 resource "aws_api_gateway_deployment" "current" {
-  rest_api_id = "${aws_api_gateway_rest_api.current.id}"
-  stage_name  = "${var.config["branch"]}"
-  description = "Deployed at ${timestamp()}"
+  rest_api_id       = "${aws_api_gateway_rest_api.current.id}"
+  stage_name        = "${var.config["branch"]}"
+  description       = "Deployed at ${timestamp()}"
   stage_description = "Deployed at ${timestamp()}"
 
   # Necessary to work with custom domain
@@ -34,18 +40,18 @@ data "aws_acm_certificate" "selected" {
 }
 
 resource "aws_api_gateway_domain_name" "current" {
-  domain_name = "${var.config["branch"] == "master" ? "${var.subdomain}.bombbomb.io" : "${var.sub_subdomain == "" ? "" : "${var.sub_subdomain}."}${var.config["branch"]}-${var.subdomain}.dev.bombbomb.io" }"
+  domain_name     = "${var.config["branch"] == "master" ? "${var.sub_subdomain == "" ? "" : "${var.sub_subdomain}."}${var.subdomain}.bombbomb.io" : "${var.sub_subdomain == "" ? "" : "${var.sub_subdomain}."}${var.config["branch"]}-${var.subdomain}.dev.bombbomb.io" }"
   certificate_arn = "${var.certificate_arn == "" ? "${data.aws_acm_certificate.selected.arn}" : "${var.certificate_arn}" }"
 }
 
 resource "aws_route53_record" "current" {
   zone_id = "${var.config["app_zone_id"]}"
-  name = "${aws_api_gateway_domain_name.current.domain_name}"
-  type = "A"
+  name    = "${aws_api_gateway_domain_name.current.domain_name}"
+  type    = "A"
 
   alias {
-    name = "${aws_api_gateway_domain_name.current.cloudfront_domain_name}"
-    zone_id = "${aws_api_gateway_domain_name.current.cloudfront_zone_id}"
+    name                   = "${aws_api_gateway_domain_name.current.cloudfront_domain_name}"
+    zone_id                = "${aws_api_gateway_domain_name.current.cloudfront_zone_id}"
     evaluate_target_health = false
   }
 }
@@ -57,19 +63,19 @@ resource "aws_api_gateway_base_path_mapping" "current" {
 }
 
 resource "aws_api_gateway_method" "current" {
-  rest_api_id = "${aws_api_gateway_rest_api.current.id}"
-  resource_id = "${aws_api_gateway_resource.all_paths.id}"
-  http_method = "ANY"
+  rest_api_id   = "${aws_api_gateway_rest_api.current.id}"
+  resource_id   = "${aws_api_gateway_resource.all_paths.id}"
+  http_method   = "ANY"
   authorization = "${var.authorization}"
 }
 
 resource "aws_api_gateway_integration" "current" {
-  rest_api_id = "${aws_api_gateway_rest_api.current.id}"
-  resource_id = "${aws_api_gateway_resource.all_paths.id}"
-  http_method = "ANY"
+  rest_api_id             = "${aws_api_gateway_rest_api.current.id}"
+  resource_id             = "${aws_api_gateway_resource.all_paths.id}"
+  http_method             = "ANY"
   integration_http_method = "POST"
-  type = "AWS_PROXY"
-  uri = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.current.arn}/invocations"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.current.arn}/invocations"
 }
 
 resource "aws_api_gateway_method_response" "200" {
@@ -77,8 +83,14 @@ resource "aws_api_gateway_method_response" "200" {
   resource_id = "${aws_api_gateway_resource.all_paths.id}"
   http_method = "${aws_api_gateway_integration.current.http_method}"
   status_code = "200"
-  response_models = { "application/json" = "Empty" }
-  response_parameters = { "method.response.header.Access-Control-Allow-Origin" = true }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
 }
 
 resource "aws_api_gateway_method_response" "202" {
@@ -86,8 +98,14 @@ resource "aws_api_gateway_method_response" "202" {
   resource_id = "${aws_api_gateway_resource.all_paths.id}"
   http_method = "${aws_api_gateway_integration.current.http_method}"
   status_code = "202"
-  response_models = { "application/json" = "Empty" }
-  response_parameters = { "method.response.header.Access-Control-Allow-Origin" = true }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
 }
 
 resource "aws_api_gateway_method_response" "400" {
@@ -95,8 +113,14 @@ resource "aws_api_gateway_method_response" "400" {
   resource_id = "${aws_api_gateway_resource.all_paths.id}"
   http_method = "${aws_api_gateway_method.current.http_method}"
   status_code = "400"
-  response_models = { "application/json" = "Error" }
-  response_parameters = { "method.response.header.Access-Control-Allow-Origin" = true }
+
+  response_models = {
+    "application/json" = "Error"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
 }
 
 resource "aws_api_gateway_method_response" "500" {
@@ -104,6 +128,12 @@ resource "aws_api_gateway_method_response" "500" {
   resource_id = "${aws_api_gateway_resource.all_paths.id}"
   http_method = "${aws_api_gateway_method.current.http_method}"
   status_code = "500"
-  response_models = { "application/json" = "Error" }
-  response_parameters = { "method.response.header.Access-Control-Allow-Origin" = true }
+
+  response_models = {
+    "application/json" = "Error"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
 }
