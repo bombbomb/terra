7ee06c03ec81c2c6950e0fce142d65eb72f8ce03
bbouley <bradleyjamesbouley@gmail.com>
2018-04-30 14:07:42 -0600

    added new config to allow vpc to be included

---
+++ lambda_express_bundle_vpc/variables.tf
@@ -0,0 +1,30 @@
+variable "config" { type = "map" }
+
+variable "subdomain" {}
+variable "sub_subdomain" { default = "" }
+variable "certificate_arn" { default = "" }
+variable "lambda_role_arn" {}
+variable "lambda_path" {}
+
+variable "build_command" {
+  default = "npm install && npm run lighthouse-build --if-present"
+}
+
+variable "authorization" { default = "NONE" }
+
+variable "lambda_runtime" { default = "nodejs6.10"}
+variable "lambda_memory_size" { default = "128" }
+variable "lambda_timeout" { default = "30" }
+variable "lambda_environment_variables" {
+  type = "map"
+  default = {
+    CREATED_BY = "lambda_express_bundle"
+  }
+}
+variable "description" { default = "" }
+
+variable "request_parameters" {
+  type = "map"
+  default = {
+  }
+}
