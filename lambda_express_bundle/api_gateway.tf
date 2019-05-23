2dc9c44914335c277651541140f64479bd101193
briankopp <briankopp.usa@gmail.com>
2019-05-23 10:22:15 -0600

    Require deployment resource wait on integration resource

--- lambda_express_bundle/api_gateway.tf
+++ lambda_express_bundle/api_gateway.tf
@@ -29,7 +29,10 @@ resource "aws_api_gateway_deployment" "current" {
     create_before_destroy = true
   }
 
-  depends_on = ["aws_api_gateway_method.current"]
+  depends_on = [
+    "aws_api_gateway_method.current",
+    "aws_api_gateway_integration.current"
+  ]
 }
 
 data "aws_acm_certificate" "selected" {
