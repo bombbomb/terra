ca6be8625d442cc9aa7efddd0ebd0e369675a80c
Brian Kopp <briankopp.usa@gmail.com>
2019-07-22 11:59:15 -0600

    CORE-403 Add rancher route 53 module (#12)

---
+++ rancher_route53/main.tf
@@ -0,0 +1,21 @@
+variable "parent_dns" {}
+variable "child_full_dns" {}
+variable "ingress_dns" {}
+variable "ttl" { default = "30" }
+
+data "aws_route53_zone" "main" {
+    name = "${var.hosted_zone}"
+}
+resource "aws_route53_zone" "app_zone" {
+    name = "${var.child_full_dns}"
+}
+resource "aws_route53_record" "cname" {
+    zone_id = "${aws_route53_zone.main.zone_id}"
+    name    = "${var.child_full_dns}"
+    type    = "CNAME"
+    ttl     = "${var.ttl}"
+    records = ["${var.ingress_dns}"]
+}
+output "app_zone_id" {
+    value = "${aws_rotue53_zone.app_zone.zone_id}"
+}
