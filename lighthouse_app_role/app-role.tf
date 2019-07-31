c5b1ae2bf22918a273f31b29a70d21c4e77b9782
briankopp <briankopp.usa@gmail.com>
2019-07-31 12:14:39 -0600

    Use the document syntax because of 0.11 bug

--- lighthouse_app_role/app-role.tf
+++ lighthouse_app_role/app-role.tf
@@ -1,25 +1,18 @@
+data "aws_iam_policy_document" "app_role_policy_document" {
+    statement {
+        actions = ["sts.AssumeRole"]
+        principals = {
+            type = "Service"
+            identifiers = ["ec2.amazonaws.com", "ecs-tasks.amazonaws.com", "lambda.amazonaws.com"]
+        }
+        principals = {
+            type = "AWS"
+            identifiers = "${concat("${var.trust_relationship_roles}","${list("arn:aws:iam::${var.server_account_id}:root", "arn:aws:iam::${var.main_account_id}:root")}")}"
+        }
+    }
+}
+
 resource "aws_iam_role" "app_role" {
     name = "${var.prefix}-AppRole"
-    assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-                "Service": "$${concat([
-                    "ec2.amazonaws.com",
-                    "ecs-tasks.amazonaws.com",
-                    "lambda.amazonaws.com"
-                ],"${var.trust_relationship_roles}")}",
-                "AWS": [
-                    "arn:aws:iam::${var.server_account_id}:root",
-                    "arn:aws:iam::${var.main_account_id}:root"
-                ]
-            },
-            "Effect": "Allow"
-        }
-    ]
-}
-EOF
+    assume_role_policy = "${data.aws_iam_policy_document.app_role_policy_document.json}"
 }
