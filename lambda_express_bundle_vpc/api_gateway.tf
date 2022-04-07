8453064282188d0638f3b56a5df00d8093257b5c
Brian Kopp <briankopp.usa@gmail.com>
2022-04-07 10:33:17 -0600

    Remove quotes

--- lambda_express_bundle_vpc/api_gateway.tf
+++ lambda_express_bundle_vpc/api_gateway.tf
@@ -9,18 +9,18 @@ data "aws_iam_role" "lambda_role" {
 # API Gateway
 resource "aws_api_gateway_rest_api" "current" {
   name        = "${var.config["prefix"]}-${var.subdomain}"
-  description = "${var.description}"
+  description = var.description
 }
 
 resource "aws_api_gateway_resource" "all_paths" {
-  rest_api_id = "${aws_api_gateway_rest_api.current.id}"
-  parent_id   = "${aws_api_gateway_rest_api.current.root_resource_id}"
+  rest_api_id = aws_api_gateway_rest_api.current.id
+  parent_id   = aws_api_gateway_rest_api.current.root_resource_id
   path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_deployment" "current" {
-  rest_api_id       = "${aws_api_gateway_rest_api.current.id}"
-  stage_name        = "${var.config["branch"]}"
+  rest_api_id       = aws_api_gateway_rest_api.current.id
+  stage_name        = var.config["branch"]
   description       = "Deployed at ${timestamp()}"
   stage_description = "Deployed at ${timestamp()}"
 
@@ -30,8 +30,8 @@ resource "aws_api_gateway_deployment" "current" {
   }
 
   depends_on = [
-    "aws_api_gateway_method.current",
-    "aws_api_gateway_integration.current"
+    aws_api_gateway_method.current,
+    aws_api_gateway_integration.current
   ]
 }
 
@@ -41,38 +41,38 @@ data "aws_acm_certificate" "selected" {
 }
 
 resource "aws_api_gateway_domain_name" "current" {
-  domain_name     = "${var.config["branch"] == "master" ? "${var.subdomain}.bombbomb.io" : "${var.config["branch"]}-${var.subdomain}.dev.bombbomb.io" }"
+  domain_name     = var.config["branch"] == "master" ? "${var.subdomain}.bombbomb.io" : "${var.config["branch"]}-${var.subdomain}.dev.bombbomb.io" 
   certificate_arn = data.aws_acm_certificate.selected.arn
 }
 
 resource "aws_route53_record" "current" {
-  zone_id = "${var.config["app_zone_id"]}"
-  name    = "${aws_api_gateway_domain_name.current.domain_name}"
+  zone_id = var.config["app_zone_id"]
+  name    = aws_api_gateway_domain_name.current.domain_name
   type    = "A"
 
   alias {
-    name                   = "${aws_api_gateway_domain_name.current.cloudfront_domain_name}"
-    zone_id                = "${aws_api_gateway_domain_name.current.cloudfront_zone_id}"
+    name                   = aws_api_gateway_domain_name.current.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.current.cloudfront_zone_id
     evaluate_target_health = false
   }
 }
 
 resource "aws_api_gateway_base_path_mapping" "current" {
-  api_id      = "${aws_api_gateway_rest_api.current.id}"
-  stage_name  = "${aws_api_gateway_deployment.current.stage_name}"
-  domain_name = "${aws_api_gateway_domain_name.current.domain_name}"
+  api_id      = aws_api_gateway_rest_api.current.id
+  stage_name  = aws_api_gateway_deployment.current.stage_name
+  domain_name = aws_api_gateway_domain_name.current.domain_name
 }
 
 resource "aws_api_gateway_method" "current" {
-  rest_api_id   = "${aws_api_gateway_rest_api.current.id}"
-  resource_id   = "${aws_api_gateway_resource.all_paths.id}"
+  rest_api_id   = aws_api_gateway_rest_api.current.id
+  resource_id   = aws_api_gateway_resource.all_paths.id
   http_method   = "ANY"
-  authorization = "${var.authorization}"
+  authorization = var.authorization
 }
 
 resource "aws_api_gateway_integration" "current" {
-  rest_api_id             = "${aws_api_gateway_rest_api.current.id}"
-  resource_id             = "${aws_api_gateway_resource.all_paths.id}"
+  rest_api_id             = aws_api_gateway_rest_api.current.id
+  resource_id             = aws_api_gateway_resource.all_paths.id
   http_method             = "ANY"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -81,16 +81,16 @@ resource "aws_api_gateway_integration" "current" {
 
 # handle root requests
 resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.current.id}"
-  resource_id   = "${aws_api_gateway_rest_api.current.root_resource_id}"
+  rest_api_id   = aws_api_gateway_rest_api.current.id
+  resource_id   = aws_api_gateway_rest_api.current.root_resource_id
   http_method   = "ANY"
-  authorization = "${var.authorization}"
+  authorization = var.authorization
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.current.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.current.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -98,9 +98,9 @@ resource "aws_api_gateway_integration" "lambda_root" {
 }
 
 resource "aws_api_gateway_method_response" "200" {
-  rest_api_id = "${aws_api_gateway_rest_api.current.id}"
-  resource_id = "${aws_api_gateway_resource.all_paths.id}"
-  http_method = "${aws_api_gateway_integration.current.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.current.id
+  resource_id = aws_api_gateway_resource.all_paths.id
+  http_method = aws_api_gateway_integration.current.http_method
   status_code = "200"
 
   response_models = {
@@ -113,9 +113,9 @@ resource "aws_api_gateway_method_response" "200" {
 }
 
 resource "aws_api_gateway_method_response" "202" {
-  rest_api_id = "${aws_api_gateway_rest_api.current.id}"
-  resource_id = "${aws_api_gateway_resource.all_paths.id}"
-  http_method = "${aws_api_gateway_integration.current.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.current.id
+  resource_id = aws_api_gateway_resource.all_paths.id
+  http_method = aws_api_gateway_integration.current.http_method
   status_code = "202"
 
   response_models = {
@@ -128,9 +128,9 @@ resource "aws_api_gateway_method_response" "202" {
 }
 
 resource "aws_api_gateway_method_response" "400" {
-  rest_api_id = "${aws_api_gateway_rest_api.current.id}"
-  resource_id = "${aws_api_gateway_resource.all_paths.id}"
-  http_method = "${aws_api_gateway_method.current.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.current.id
+  resource_id = aws_api_gateway_resource.all_paths.id
+  http_method = aws_api_gateway_method.current.http_method
   status_code = "400"
 
   response_models = {
@@ -143,9 +143,9 @@ resource "aws_api_gateway_method_response" "400" {
 }
 
 resource "aws_api_gateway_method_response" "500" {
-  rest_api_id = "${aws_api_gateway_rest_api.current.id}"
-  resource_id = "${aws_api_gateway_resource.all_paths.id}"
-  http_method = "${aws_api_gateway_method.current.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.current.id
+  resource_id = aws_api_gateway_resource.all_paths.id
+  http_method = aws_api_gateway_method.current.http_method
   status_code = "500"
 
   response_models = {
--- lambda_express_bundle_vpc/variables.tf
+++ lambda_express_bundle_vpc/variables.tf
@@ -1,35 +1,35 @@
-variable "config" { type = "map" }
+variable "config" { type = map }
 
 variable "subdomain" {}
 variable "lambda_role_arn" {}
 variable "lambda_path" {}
 
 variable "build_command" {
-  default = "npm install && npm run lighthouse-build --if-present"
+  default = "npm ci && npm run lighthouse-build --if-present"
 }
 
 variable "authorization" { default = "NONE" }
 
 variable "lambda_handler" { default = "index.handler"}
-variable "lambda_runtime" { default = "nodejs10.x"}
+variable "lambda_runtime" { default = "nodejs14.x"}
 variable "lambda_memory_size" { default = "128" }
 variable "lambda_timeout" { default = "30" }
 variable "lambda_environment_variables" {
-  type = "map"
+  type = map
   default = {
     CREATED_BY = "lambda_express_bundle_vpc"
   }
 }
 variable "lambda_security_group_ids" {
-  type = "list"
+  type = list
 }
 variable "lambda_subnet_ids" {
-  type = "list"
+  type = list
 }
 variable "description" { default = "" }
 
 variable "request_parameters" {
-  type = "map"
+  type = map
   default = {
   }
 }
